631388f6c8c6e88a548e65b80977a4c46c4a45af
Karla Robinson <karlarobinson5@gmail.com>
2018-09-28 20:06:56 -0500

    add movie and comment models

---
+++ db/migrate/20180929005752_create_movies.rb
@@ -0,0 +1,15 @@
+class CreateMovies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :genre
+      t.text :cast
+      t.string :director
+      t.integer :prod_year
+      t.integer :rating
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
